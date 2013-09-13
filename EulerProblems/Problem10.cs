1e43f9155aad92225920647e2fbc6b81fd880cbd
Aidan Casey <aidancasey@gmail.com>
2013-09-13 19:33:30 +1000

    problems 9 & 10 solved

---
+++ EulerProblems/Problem10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using EulerProblems.Extensions;
+
+namespace EulerProblems
+{
+    public class Problem10 : ISolveProblemManyWays
+    {
+        public string Solve()
+        {
+            long total = 0;
+           for(int i= 2; i<2000000;i++)
+            {
+                if (i.IsPrime())
+                {
+                    total += i;
+                }
+
+            }
+
+            return total.ToString();
+
+        }
+
+        public string LinqSolve()
+        {
+            return "not implemented";
+        }
+
+        public string AlgebraSolve()
+        {
+            //to do implement Sieve of Eratosthenes
+
+            //   http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+            
+            return "not implemented";
+
+        }
+    }
+}
---
+++ EulerProblems/Problem9.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EulerProblems
+{
+    public class Problem9 : IProblem
+    {
+        #region IProblem Members
+
+
+//        A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//          a2 + b2 = c2
+//          For example, 32 + 42 = 9 + 16 = 25 = 52.
+//          There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//          Find the product abc.
+
+
+        // c = 1000 -b -a
+
+        public string Solve()
+        {
+            int a;
+            int b;
+            int c=0;
+
+            bool found = false;
+
+            for ( a = 1; a < 1000; a++)
+            {
+                for ( b = 1; b < 1000; b++)
+                {
+
+                    c = 1000 - a - b;
+
+                    if ((a*a) + (b*b) == (c*c))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    return (a * b * c).ToString();
+                }
+            }
+            return string.Empty;
+
+        }
+
+        #endregion
+    }
+}
